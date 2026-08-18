3f8715fdbe6f3e3c94f53cba60b7d78ca97be1db
Piyush Jadhav <piyushsj02@gmail.com>
2026-08-18 16:01:03 +0530

    commit

--- src/main/java/com/info/jobapp/job/Job.java
+++ src/main/java/com/info/jobapp/job/Job.java
@@ -1,21 +1,29 @@
-package com.info.jobapp.model;
+package com.info.jobapp.job;
 
 
+import com.info.jobapp.company.Company;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "job_table")
 public class Job {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private String minSalary;
     private String maxSalary;
     private String location;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="company_id", nullable=false)
+    private Company company;
 }
